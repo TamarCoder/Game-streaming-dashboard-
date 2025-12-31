26b13c325399cedeb8d207703fac90a30155890f
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-12-31 12:51:18 +0400

    Remove package-lock.json and next.config.js from repository

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    silenceDeprecations: ['legacy-js-api'],
-  },
-}
-
-module.exports = nextConfig
